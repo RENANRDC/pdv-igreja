42942ba77add80d02332d26eb16057d93612d1a9
RENANRDC <167394060+RENANRDC@users.noreply.github.com>
2026-04-10 17:12:33 -0300

    fix(layout): corrige altura no mobile usando dvh e elimina gap entre conteúdo e footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className="h-screen flex flex-col overflow-hidden bg-gray-900 text-white">
 
         {/* CONTEÚDO */}
-        <main className="flex-1 min-h-0 flex flex-col">
+        <main className="flex-1 flex flex-col min-h-0">
           {children}
         </main>
 
